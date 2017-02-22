826be328c2caf2aae9c3cdc275b6a942086778f8
Musaev Ruslan <musya.wow@gmail.com>
2017-02-22 23:17:29 +0300

    animation el, scss

--- theme/js/js.js
+++ theme/js/js.js
@@ -1,25 +1,32 @@
 (function ($) {
 
-  if (typeof Drupal != 'undefined') {
-    Drupal.behaviors.wcportalMobileTheme = {
-      attach: function (context, settings) {
-        init();
-      },
+$(window).load(function(){
+  initElmsAnimation();
+});
 
-      completedCallback: function () {
-        // Do nothing. But it's here in case other modules/themes want to override it.
+  function initElmsAnimation() {
+    var $elms = $('.el-with-animation');
+    var animationEnd = [];
+
+    $(window).on('resize scroll', checkScroll);
+
+    checkScroll();
+
+    function checkScroll() {
+      if (animationEnd.length === $elms.length) return;
+
+      for (var i = 0; i < $elms.length; i++) {
+        var $currentEl = $elms.eq(i);
+        if (!$currentEl.hasClass('animating-end') && $(window).height() + $(window).scrollTop() > $currentEl.offset().top + $currentEl.height() / 2 + 50) {
+          animate($currentEl);
+        }
       }
     }
-  }
 
-  $(function () {
-    if (typeof Drupal == 'undefined') {
-      init();
+    function animate(el) {
+      el.addClass('animating-end');
+      animationEnd.push(1);
     }
-  });
-
-  function init() {
-
   }
 
 })(jQuery);
