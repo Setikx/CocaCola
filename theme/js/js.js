3973b7731bfe7cd4fa01e88e994a361489b92542
Setikx <seda.wow@gmail.com>
2017-02-23 23:46:23 +0300

    last push

---
+++ theme/js/js.js
@@ -0,0 +1,40 @@
+(function ($) {
+
+$(window).load(function(){
+ // initElmsAnimation();
+  name();
+});
+
+  function initElmsAnimation() {
+    var $elms = $('.el-with-animation');
+    var animationEnd = [];
+
+    $(window).on('resize scroll', checkScroll);
+
+    checkScroll();
+
+    function checkScroll() {
+      if (animationEnd.length === $elms.length) return;
+
+      for (var i = 0; i < $elms.length; i++) {
+        var $currentEl = $elms.eq(i);
+        if (!$currentEl.hasClass('animating-end') && $(window).height() + $(window).scrollTop() > $currentEl.offset().top + $currentEl.height() / 2 + 50) {
+          animate($currentEl);
+        }
+      }
+    }
+
+    function animate(el) {
+      el.addClass('animating-end');
+      animationEnd.push(1);
+    }
+  }
+
+  function name(){
+
+  }
+
+})(jQuery);
+
+
+
