9dec48a4f3f93f191cc8e1e01b8d88c565b4634e
Setikx <seda.wow@gmail.com>
2017-02-23 23:20:03 +0300

    'trying_slider'

---
+++ theme/js/slider.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Set on 23.02.2017.
+ */
+$(window).load(function() {
+    $('.flexslider').flexslider({
+        animation: "slide",
+        animationLoop: false,
+        itemWidth: 352,
+        itemMargin: 3
+    });
+});
